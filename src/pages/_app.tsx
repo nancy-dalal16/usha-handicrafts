9525fa776d539200a8ca3a7a2769b3cdecbe50f1
nancy-dalal16 <nancydalal16@gmail.com>
2021-09-17 16:28:04 +0530

    google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,8 +30,21 @@ export default function CustomApp({ Component, pageProps }) {
       });
     }
   }, []);
+
   return (
     <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      ></script>
+      <script>
+        {` window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
+      </script>
       <SearchProvider>
         <StickyProvider>
           <DrawerProvider>
