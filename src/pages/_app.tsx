876ede06822d5a02de803dd568e38d6c7d9fa32f
nancy-dalal16 <nancydalal16@gmail.com>
2021-09-17 16:41:11 +0530

    changes in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -35,15 +35,17 @@ export default function CustomApp({ Component, pageProps }) {
     <>
       {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
       <script
-        async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      ></script>
-      <script>
-        {` window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+      />
 
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
       </script>
       <SearchProvider>
         <StickyProvider>
